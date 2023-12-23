e2c6ed69a3e9f78fe4c0e3b1a9e67d9edbc9d7ed
Sayman Rifat <saymanrifat@gmail.com>
2023-12-23 15:36:23 +0600

    Initial Commit

---
+++ PersonalJournalApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalJournalApp
+//
+//  Created by Sayman Rifat on 12/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalJournalApp/PersonalJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalJournalAppApp.swift
+//  PersonalJournalApp
+//
+//  Created by Sayman Rifat on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PersonalJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
